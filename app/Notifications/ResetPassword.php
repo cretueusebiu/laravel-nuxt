801807ab134d73438ae813553f941f17d8a715d8
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2018-01-10 11:17:15 +0200

    Remove duplicate code

--- app/Notifications/ResetPassword.php
+++ app/Notifications/ResetPassword.php
@@ -8,35 +8,6 @@ use Illuminate\Auth\Notifications\ResetPassword as Notification;
 class ResetPassword extends Notification
 {
     /**
-     * The password reset token.
-     *
-     * @var string
-     */
-    public $token;
-
-    /**
-     * Create a notification instance.
-     *
-     * @param  string  $token
-     * @return void
-     */
-    public function __construct($token)
-    {
-        $this->token = $token;
-    }
-
-    /**
-     * Get the notification's channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array|string
-     */
-    public function via($notifiable)
-    {
-        return ['mail'];
-    }
-
-    /**
      * Build the mail representation of the notification.
      *
      * @param  mixed  $notifiable
