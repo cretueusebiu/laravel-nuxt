4e9e8fb6a36f90be95d44c9703aca0fe49d2a00e
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2018-01-16 20:38:07 +0200

    Format

--- client/plugins/i18n.js
+++ client/plugins/i18n.js
@@ -4,7 +4,8 @@ import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 
 const i18n = new VueI18n({
-  locale: 'en', messages: {}
+  locale: 'en',
+  messages: {}
 })
 
 export default async ({ app, store }) => {
