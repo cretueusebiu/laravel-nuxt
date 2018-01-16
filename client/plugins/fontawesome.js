c7ccaa77883bc1cfb1b6ab9e1c6913175a5e964e
Cretu Eusebiu <cretu.eusebiu@gmail.com>
2018-01-16 19:42:33 +0200

    Import shakable fontawesome module

--- client/plugins/fontawesome.js
+++ client/plugins/fontawesome.js
@@ -2,13 +2,15 @@ import Vue from 'vue'
 import fontawesome from '@fortawesome/fontawesome'
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 
-// import { } from '@fortawesome/fontawesome-free-regular'
+// import { } from '@fortawesome/fontawesome-free-regular/shakable.es'
 
 import {
   faUser, faLock, faSignOutAlt, faCog
-} from '@fortawesome/fontawesome-free-solid'
+} from '@fortawesome/fontawesome-free-solid/shakable.es'
 
-import { faGithub } from '@fortawesome/fontawesome-free-brands'
+import {
+  faGithub
+} from '@fortawesome/fontawesome-free-brands/shakable.es'
 
 fontawesome.library.add(
   faUser, faLock, faSignOutAlt, faCog, faGithub
